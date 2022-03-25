b35da9473a8385a91d3bd828d94105eda8e7998e
RidwanSeptiawan <ridwanseptiawan7@gmail.com>
2022-03-25 14:52:32 +0700

    Menambahkan Components

--- tugas/src/App.js
+++ tugas/src/App.js
@@ -1,14 +1,18 @@
-import data from './single-sample';
+import data from './components/single-sample';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <input type="text" placeholder="Search" />
+      <button class="src">Search</button>
+      <br />
+      <br/>
       <header className="App-header">
       <img src={data.album.images[1].url} className="App-logo" alt="logo" />
         <h2>{data.album.name}</h2>
         <h3>{data.album.artists[0].name}</h3>
-        <button>Select</button>
+        <button class="select">Select</button>
         {/* <a
           className="App-link"
           href="https://reactjs.org"
